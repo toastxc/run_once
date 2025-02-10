1764536c32172d064d42f90a6a52b9941bbd0db6
ToastXC <100072983+toastxc@users.noreply.github.com>
2025-02-10 16:29:04 +0800

    added if method

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,14 @@ impl RunOnce {
             stuff()
         }
     }
+    pub fn run_once_if(&mut self, i: i32) -> bool {
+        if !self.0.contains(&i) {
+            self.0.insert(i);
+            true
+        } else {
+            false
+        }
+    }
 }
 
 #[cfg(test)]
@@ -25,6 +33,18 @@ mod test {
             });
         }
 
+        assert_eq!(value, 1);
+    }
+
+    #[test]
+    fn test2() {
+        let mut data = RunOnce::default();
+        let mut value = 0;
+        for _ in 0..5 {
+            if data.run_once_if(1) {
+                value += 1;
+            }
+        }
         assert_eq!(value, 1);
     }
 }
